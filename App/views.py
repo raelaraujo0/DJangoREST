de9e1c593d261015fd4d0ad2e4eadbcab53bf108
raelaraujo0 <raellyedson1@gmail.com>
2024-07-31 10:05:48 -0300

    esqueci os parenteses na funcao isvalid

--- App/views.py
+++ App/views.py
@@ -12,7 +12,7 @@ def books_view(request):
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = BooksSerial(data=request.data) 
-        if serializer.is_valid:
+        if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = HTTP_400_BAD_REQUEST)
+        return Response(serializer.erros, status = HTTP_400_BAD_REQUEST)
